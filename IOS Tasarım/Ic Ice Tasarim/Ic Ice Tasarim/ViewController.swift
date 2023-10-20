b3ce9bd0356656270a02a22d2156d9e7749debab
Sena Nur ERDEM <98752496+senaerdem@users.noreply.github.com>
2023-10-20 20:41:14 +0300

    IOS Tasarım
    auto layout - iç içe tasarım yapısı ve çoklu ekran desteği

---
+++ IOS Tasarım/Ic Ice Tasarim/Ic Ice Tasarim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ic Ice Tasarim
+//
+//  Created by Sena Nur Erdem on 10.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
