075ca012cab00b52fcf27ad09364f1d802f3f656
Sena Nur ERDEM <98752496+senaerdem@users.noreply.github.com>
2023-10-20 20:34:02 +0300

    IOS Giriş
    ilk uygulamanın çalıştırılması

---
+++ ilkUygulama/ilkUygulama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ilkUygulama
+//
+//  Created by Sena Nur Erdem on 5.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
