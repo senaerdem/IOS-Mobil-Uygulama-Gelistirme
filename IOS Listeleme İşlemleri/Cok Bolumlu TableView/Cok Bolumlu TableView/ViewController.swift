9a685bee769784684fde546cf1dc7fcea45871eb
Sena Nur ERDEM <98752496+senaerdem@users.noreply.github.com>
2023-10-24 20:22:20 +0300

    IOS Listeleme İşlemleri
    çok bölümlü tabela view

---
+++ IOS Listeleme İşlemleri/Cok Bolumlu TableView/Cok Bolumlu TableView/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Cok Bolumlu TableView
+//
+//  Created by Sena Nur Erdem on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var bolumler = ["Meyveler","Renkler","Mevsimler"]
+    var bolumVerileri = [["Elma","Muz","Ayva","Çilek"],["Kırmızı","Mavi","Yeşil"],["Yaz","Kış"]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return bolumler.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bolumVerileri[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return bolumler[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hucreadi",for: indexPath)
+        
+        cell.textLabel?.text = bolumVerileri[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(bolumler[indexPath.section]) bölümünde \(bolumVerileri[indexPath.section][indexPath.row]) seçildi")
+    }
+}
